bbb8f8ddaeb4b99d33a062f505059da8aa42e973
andy <coeu5a@protonmail.com>
2021-12-10 10:38:14 +0000

    add network and wallet helpers

---
+++ helpers/index.ts
@@ -0,0 +1,4 @@
+import { addNetwork, NETWORK_ID, BLOCK_EXPLORER_URL } from "./network";
+import { readyToTransact } from "./wallet";
+
+export { addNetwork, NETWORK_ID, BLOCK_EXPLORER_URL, readyToTransact };
