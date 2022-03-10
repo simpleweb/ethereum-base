5e2729692e612c20afa82aa99403fb0b1d191232
Andy <andy.kitt@simpleweb.co.uk>
2022-03-10 16:25:42 +0000

    remove walletconnect from onboard

--- services/onboard.ts
+++ services/onboard.ts
@@ -1,9 +1,6 @@
 import Onboard from "bnc-onboard";
 
-const networkId = parseInt(
-  process.env.NEXT_PUBLIC_NETWORK_ID || "80001",
-  10
-);
+const networkId = parseInt(process.env.NEXT_PUBLIC_NETWORK_ID || "80001", 10);
 const networkName = process.env.NEXT_PUBLIC_NETWORK_NAME || "mumbai";
 const dappId = process.env.NEXT_PUBLIC_BNC_API_KEY;
 const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL;
@@ -17,15 +14,7 @@ export default function initOnboard(subscriptions: any) {
     darkMode: false,
     subscriptions,
     walletSelect: {
-      wallets: [
-        { walletName: "metamask" },
-        {
-          walletName: "walletConnect",
-          rpc: {
-            ["80001"]: RPC_URL,
-          },
-        },
-      ],
+      wallets: [{ walletName: "metamask" }],
     },
     walletCheck: [
       { checkName: "connect" },
