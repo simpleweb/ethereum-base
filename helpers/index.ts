6e9361853cef7f672bfa1185760b3a7fbace39bd
Andy <andy.kitt@simpleweb.co.uk>
2022-03-15 15:42:11 +0000

    update wallet helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,5 +1,5 @@
 import { callContract } from "./contract";
-import { addNetwork, NETWORK_ID, BLOCK_EXPLORER_URL } from "./network";
+import { addNetwork, BLOCK_EXPLORER_URL, NETWORK_ID } from "./network";
 import { readyToTransact } from "./wallet";
 
 export {
--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -1,8 +1,22 @@
-async function readyToTransact(onboard: any, previouslySelectedWallet: any) {
-  if (onboard) {
-    await onboard.walletSelect(previouslySelectedWallet);
-    await onboard.walletCheck();
+import type {
+  ConnectOptions,
+  SetChainOptions,
+  WalletState,
+} from "@web3-onboard/core";
+
+async function readyToTransact(
+  wallet: WalletState,
+  connect: () => void,
+  setChain: (options: SetChainOptions) => void
+) {
+  if (!wallet) {
+    const walletSelected = await connect();
+    if (!walletSelected) return false;
   }
+  // prompt user to switch to Rinkeby for test
+  await setChain({ chainId: process.env.NEXT_PUBLIC_CHAIN_ID });
+
+  return true;
 }
 
 export { readyToTransact };
